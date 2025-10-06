7c4874eb29dfd6597376cb585db0212ca713e68a
Vchen7629 <chenvincent7629@gmail.com>
2025-10-05 19:11:42 -0700

    refactor: change <a> buttons for routing to <Link> to prevent full page reloads losing user state

--- frontend/src/features/navbar/components/Header.tsx
+++ frontend/src/features/navbar/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Switch } from "@/features/navbar/components/darkLightModeSwitch"
+import { Link } from "react-router"
 
 const Header = () => {
     
@@ -6,9 +7,9 @@ const Header = () => {
         <header className="flex justify-between w-screen h-[10vh] bg-[#879B9E] px-[2.5%]">
             <div className="flex h-full items-center space-x-[2vw] w-fit"> 
                 <h2 className="text-3xl font-bold text-white">CineSense</h2>
-                <a href="/" className="text-xl text-white font-medium hover:text-gray-500">Home</a>
-                <a href="/add-movies" className="text-xl text-white font-medium hover:text-gray-500">Add Movies</a>
-                <a href="/recommendations" className="text-xl text-white font-medium hover:text-gray-500">Recommendations</a>
+                <Link to="/" className="text-xl text-white font-medium hover:text-gray-500">Home</Link>
+                <Link to="/add-movies" className="text-xl text-white font-medium hover:text-gray-500">Add Movies</Link>
+                <Link to="/recommendations" className="text-xl text-white font-medium hover:text-gray-500">Recommendations</Link>
             </div>
             <div className="flex h-full items-center space-x-[2vw] w-fit"> 
                 <div className="flex items-center space-x-2">
@@ -16,9 +17,9 @@ const Header = () => {
                 </div>
                 <div className="flex space-x-[1rem] items-center"> 
                     <div className="bg-[#D9D9D9] h-[2.5rem] w-[2.5rem] rounded-full"/>
-                    <a href="/profile" className="text-xl text-white font-medium hover:text-gray-500">
+                    <Link to="/profile" className="text-xl text-white font-medium hover:text-gray-500">
                         User Name
-                    </a>
+                    </Link>
                 </div>
             </div>
         </header>
