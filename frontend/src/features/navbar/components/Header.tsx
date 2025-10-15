95a44551abd9066e92eac854546ed10c3ae5110a
Vchen7629 <chenvincent7629@gmail.com>
2025-10-15 00:05:38 -0700

    feat: Updated header to change based on if the user is logged in or not

--- frontend/src/features/navbar/components/Header.tsx
+++ frontend/src/features/navbar/components/Header.tsx
@@ -1,8 +1,10 @@
 import { Switch } from "@/features/navbar/components/darkLightModeSwitch"
 import { Link } from "react-router"
+import { useAuthState } from "@/app/user-store"
 
 const Header = () => {
-    
+    const { loggedIn } = useAuthState()
+
     return (
         <header className="flex justify-between w-screen h-[10vh] bg-[#879B9E] px-[2.5%]">
             <div className="flex h-full items-center space-x-[2vw] w-fit"> 
@@ -15,12 +17,22 @@ const Header = () => {
                 <div className="flex items-center space-x-2">
                     <Switch id="airplane-mode"/>
                 </div>
-                <div className="flex space-x-[1rem] items-center"> 
-                    <div className="bg-[#D9D9D9] h-[2.5rem] w-[2.5rem] rounded-full"/>
-                    <Link to="/profile" className="text-xl text-white font-medium hover:text-gray-500">
-                        User Name
-                    </Link>
-                </div>
+                <>
+                    {loggedIn && ( // Display the User name when user is logged in
+                        <div className="flex space-x-[1rem] items-center">
+                            <div className="bg-[#D9D9D9] h-[2.5rem] w-[2.5rem] rounded-full"/>
+                            <Link to="/profile" className="text-xl text-white font-medium hover:text-gray-500">
+                                User Name
+                            </Link>
+                        </div>
+                    )}
+                    {!loggedIn && ( // Display the login and signup buttons if user is not logged in
+                        <>
+                            <Link to="/login" className="text-xl text-white font-medium hover:text-gray-500">Login</Link>
+                            <Link to="/signup" className="text-xl text-white font-medium hover:text-gray-500">Sign-up</Link>
+                        </>
+                    )}
+                </>
             </div>
         </header>
     )
