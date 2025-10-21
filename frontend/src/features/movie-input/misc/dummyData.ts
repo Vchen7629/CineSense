04ffc4d9b7fadc6e6c169c8d457e02046c6c2eb4
Vchen7629 <chenvincent7629@gmail.com>
2025-10-21 16:08:06 -0700

    chore(add-movies-page): added dummy dataset for testing

---
+++ frontend/src/features/movie-input/misc/dummyData.ts
@@ -0,0 +1,11 @@
+export const dummyData = [
+    {id: 1, name: "movie 1", desc: "movie description", genres: "genre 1, genre 2, genre 3", like: false, dislike: false},
+    {id: 2, name: "movie 2", desc: "movie description", genres: "genre 1, genre 2, genre 3", like: false, dislike: false},
+    {id: 3, name: "movie 3", desc: "movie description", genres: "genre 1, genre 2, genre 3", like: false, dislike: false},
+    {id: 4, name: "movie 4", desc: "movie description", genres: "genre 1, genre 2, genre 3", like: false, dislike: false},
+    {id: 5, name: "movie 5", desc: "movie description", genres: "genre 1, genre 2, genre 3", like: false, dislike: false},
+    {id: 6, name: "movie 6", desc: "movie description", genres: "genre 1, genre 2, genre 3", like: false, dislike: false},
+    {id: 7, name: "movie 7", desc: "movie description", genres: "genre 1, genre 2, genre 3", like: false, dislike: false},
+    //{id: 8, name: "movie 8", desc: "movie description", genres: "genre 1, genre 2, genre 3", like: false, dislike: false},
+    //{id: 9, name: "movie 9", desc: "movie description", genres: "genre 1, genre 2, genre 3", like: false, dislike: false},
+]
