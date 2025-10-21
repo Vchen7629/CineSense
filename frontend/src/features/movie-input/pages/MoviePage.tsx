34ed1a53b054d46c74754ecd36f52234a8052f29
Vchen7629 <chenvincent7629@gmail.com>
2025-10-21 16:05:50 -0700

    feat(add-movies-page): Created component for displaying movie page

--- frontend/src/features/movie-input/pages/MoviePage.tsx
+++ frontend/src/features/movie-input/pages/MoviePage.tsx
@@ -1,12 +1,29 @@
 import Header from "@/features/navbar/components/Header";
+import SearchBar from "../components/searchBar";
+import DisplayAddedMovie from "../components/moviesDisplay";
+import { useState } from "react";
+import { dummyData } from "../misc/dummyData";
+import ClearButton from "../components/clearButton";
+import SaveButton from "../components/saveButton";
+import type { DummyItem } from "../types/data";
 
 export default function MovieInputPage() {
+    const [data, setData] = useState<DummyItem[]>(dummyData)
+    const [filteredData, setFilteredData] = useState<DummyItem[]>(dummyData)
+
     return (
-        <main>
+        <>
             <Header/>
-            <div className="flex h-[90vh] w-full justify-center items-center ">
-                <p className="text-white text-3xl"> Add Movies Page </p>
-            </div>
-        </main>
+            <main className="flex flex-col justify-between h-[90vh] w-full py-[5vh] px-[5vw]">
+                <section className="flex w-full h-[10%] justify-between">
+                    <SearchBar data={data} setData={setFilteredData}/>
+                    <div className="flex items-center h-full w-[12.5%] space-x-6">
+                        <ClearButton setFilteredData={setFilteredData}/>
+                        <SaveButton/>
+                    </div>
+                </section>
+                <DisplayAddedMovie movieData={filteredData} setMovieData={setData} setFilteredMovieData={setFilteredData}/>
+            </main>
+        </>
     )
 }
