e5125e287fd774c5d650a01ffda6a50fd4ac17b8
jai_s06 <jai.sanghrajka@gmail.com>
2025-10-23 16:38:25 -0700

    Added Login Page (credentials not enter-able yet)

--- frontend/src/features/user/pages/LoginPage.tsx
+++ frontend/src/features/user/pages/LoginPage.tsx
@@ -1,13 +1,54 @@
 import Header from "@/features/navbar/components/Header";
+import { Link } from "react-router";
 
 export default function LoginPage() {
-    
     return (
-        <main>
-            <Header/>
-            <div className="flex h-[90vh] w-full justify-center items-center ">
-                <p className="text-white text-3xl"> User Login Page </p>
-            </div>
-        </main>
-    )
+    <main className="bg-[#2E454D] min-h-screen">
+      <Header />
+      
+      <div className="px-8">
+        <p className="mt-15 text-white text-5xl">Log In</p>
+      </div>
+
+      {/* <div className="bg-[#D9D9D9] h-[20rem] w-[20rem] rounded-full absolute top-[300px] ml-[1260px]" />
+
+      <div className="flex bg-[#D9D9D9] h-[4rem] w-[4rem] rounded-full absolute top-[450px] ml-[1125px] items-center justify-center " >
+        <PencilIcon size = {40}/>
+      </div> */}
+
+      
+      {/* <p className="absolute top-[650px] ml-[1355px] text-white text-3xl">Username</p> */}
+
+      <p className="absolute top-[250px] ml-[30px] text-white text-2xl">Username or Email:</p>
+
+      <div className="bg-[#C7DEDB] h-[5rem] w-[64rem] absolute top-72 ml-[30px] border-4 border-[#000000] flex items-center">
+        <p className="text-black text-[20px] ml-[10px]">Username/Email Here</p>
+      </div>
+
+
+      <p className="absolute top-[400px] ml-[30px] text-white text-2xl">Password:</p>
+
+      <div className="bg-[#C7DEDB] h-[5rem] w-[64rem] absolute top-110 ml-[30px] border-4 border-[#000000] flex items-center">
+        <p className="text-black text-[20px] ml-[10px]">Password Here</p>
+      </div>
+
+      <div className="absolute top-[540px] ml-30[px] flex flex-col space-y-2">
+        <Link
+            to="/signup"
+            className="text-blue-400 text-[20px] ml-[30px] underline hover:text-blue-300 transition-colors"
+        >
+            Don't Have An Account?
+        </Link>
+      </div>
+
+
+      <Link
+        to = "/"
+        className="flex absolute bottom-70 left-10 px-8 py-4 !bg-green-800 items-center justify-center text-white text-xl font-medium w-72 border-2 border-black"
+        style={{ outline: '2px solid black', outlineOffset: '0px', borderRadius: '9999px' }}
+      >
+        Login
+      </Link>
+    </main>
+  );
 }
