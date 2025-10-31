c93f994b3b2c50fd99fd541daf1478dd8d98fe93
Vchen7629 <chenvincent7629@gmail.com>
2025-10-31 00:05:48 -0700

    feat(backend): added sqlalchemy and psycopg for connecting to database and printing tables

--- backend/user-api/app/main.py
+++ backend/user-api/app/main.py
@@ -1,8 +1,17 @@
 # Entry Point
 from fastapi import FastAPI
-from app.routes import status
+from routes import status
+from db.dbConn import engine
+from sqlalchemy import inspect
 
 app = FastAPI()
 
 # Including routes from routes folder.
 app.include_router(status.router, prefix="", tags=["status"])
+
+# printing out tables
+with engine.connect() as conn:
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+    print(tables)
+
