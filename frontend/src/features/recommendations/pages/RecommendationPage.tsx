c59c804d23aff117a148809f819036431de30425
Vchen7629 <chenvincent7629@gmail.com>
2025-11-01 22:00:04 -0700

    fixed buttons not being centered i think

--- frontend/src/features/recommendations/pages/RecommendationPage.tsx
+++ frontend/src/features/recommendations/pages/RecommendationPage.tsx
@@ -13,22 +13,19 @@ export default function RecommendationPage() {
                 <div className="bg-[#A7BCBF] h-[32.5rem] w-[30rem] absolute top-40 ml-[38px] border-[0.5rem] rounded-[15px] border-[#384A4F]">
                     <p className="text-white text-3xl absolute -top-12 left-1/2 transform -translate-x-1/2">Movie Name</p>
                     <p className="text-white text-3xl absolute bottom-25 left-1/2 transform -translate-x-1/2 text-[25px]">Genera</p>
+                    <div className="flex h-[90vh] w-full mt-[1rem] justify-between px-[15%] left-center items-center text-center">
+                        {/*This will be the like button*/}
+                        <div className="bg-[#33cc33] h-[2.8rem] w-[6rem] rounded-[15px]">
+                            {/*You can put an icon here if you want. I would need the icon files though*/}
+                        </div>
+                        {/*This will be the DISLIKE button*/}
+                        <div className="bg-[#cc3333] h-[2.8rem] w-[6rem] rounded-[15px]">
+                            {/*You can put an icon here if you want. I would need the icon files though*/}
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            {/*This will be the like button*/}
-            <div className="flex h-[90vh] w-full left-center items-center text-center">
-                 <div className="bg-[#33cc33] h-[2.8rem] w-[6rem] absolute top-150 ml-[335px] rounded-[15px]">
-                    {/*You can put an icon here if you want. I would need the icon files though*/}
-                </div>
-            </div>
-
-             {/*This will be the DISLIKE button*/}
-            <div className="flex h-[90vh] w-full left-center items-center text-center">
-                 <div className="bg-[#cc3333] h-[2.8rem] w-[6rem] absolute top-150 ml-[125px] rounded-[15px]">
-                    {/*You can put an icon here if you want. I would need the icon files though*/}
-                </div>
-            </div>
 
             {/*background flavor-image for description (Don't touch)*/}
             <div className="bg-[#A7BCBF] h-[32.5rem] w-[50.5rem] absolute top-40 ml-[600px] border-[0.5rem] rounded-[15px] border-[#384A4F]">
