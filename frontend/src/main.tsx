5cd537d9b19126040de13a3b6f3f51ef165889a5
Vchen7629 <chenvincent7629@gmail.com>
2025-11-14 22:10:31 -0800

    chore(frontend): added react query and axios library - for api calls, and set up basic query client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './shared/styles/index.css'
 import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './api/lib/queryClient.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App/>}/>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App/>}/>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
