119e72b8ff6acfc16d7d6f59a31ca5bebb417df6
Vchen7629 <chenvincent7629@gmail.com>
2025-11-14 20:41:37 -0800

    feat(backend): created routes for user, recommendations, and movies for the recommendation api

---
+++ backend/recommendation-model/api/app/routes/movies.py
@@ -0,0 +1,86 @@
+from fastapi import APIRouter, HTTPException, status, Request, Depends
+from typing import List
+from model.utils.movie_tower import MovieTower
+from db.utils.user import regenerate_user_movie_embedding
+from db.utils.movies import add_new_movie_embedding, add_new_movie_rating, add_movie_metadata, get_movie_embeddings
+from db.config.conn import get_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from pydantic import BaseModel
+from utils.dependencies import get_movie_tower
+import numpy as np
+
+router = APIRouter(prefix="/movie", tags=["movie"])
+
+class RateMovieRequest(BaseModel):
+    user_id: str
+    title: str
+    genres: List[str]
+    release_date: int
+    summary: str
+    actors: List[str]
+    director: List[str]
+    poster_path: str
+    rating: int
+
+@router.post("/rate/{imdb_id}")
+async def new_rated_movie(
+    imdb_id: str, 
+    body: RateMovieRequest,
+    movie_tower: MovieTower = Depends(get_movie_tower),
+    session: AsyncSession = Depends(get_session),
+):  
+    user_id = body.user_id
+    title = body.title
+    genres = body.genres
+    release = body.release_date
+    summary = body.summary
+    actors = body.actors
+    director = body.director
+    poster_path = body.poster_path
+    rating = body.rating
+
+    if not user_id:
+        raise HTTPException(status_code=404, detail="No user_id provided")
+    
+    if not rating:
+        raise HTTPException(status_code=404, detail="No rating provided")
+    
+    movie_embedding = movie_tower.generate_new_movie_embedding(title, genres)
+
+    await add_movie_metadata(session, imdb_id, title, genres, release, summary, actors, director, poster_path)
+
+    await add_new_movie_embedding(session, imdb_id, movie_embedding)
+
+    is_new_rating = await add_new_movie_rating(session, user_id, imdb_id, rating)
+
+    movie_embeddings_rows = await get_movie_embeddings(session, user_id)
+
+    # parse pgvector string format to numpy arrays
+    movie_embeddings = []
+    for row in movie_embeddings_rows:
+        embedding_str = row[0].strip('[]')
+        embedding = np.fromstring(embedding_str, sep=',', dtype=np.float32)
+        movie_embeddings.append(embedding)
+
+    movie_embeddings = np.array(movie_embeddings, dtype=np.float32)
+
+    # average and normalize
+    user_emb = movie_embeddings.mean(axis=0) # [512]
+    user_emb = user_emb / np.linalg.norm(user_emb)
+
+    await regenerate_user_movie_embedding(session, user_id, user_emb)
+
+    await session.commit()
+    
+    return {
+        "message": "Rating added" if is_new_rating else "Rating updated",
+        "user_id": user_id,
+        "movie_id": imdb_id,
+        "rating": rating
+    }
+
+
+
+    
+    
+    
---
+++ backend/recommendation-model/api/app/routes/user.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Request, Depends
+from model.utils.cold_start_user_tower import ColdStartUserTower
+from db.utils.user import new_user_genre_embedding, test
+from db.config.conn import get_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from utils.dependencies import get_cold_start_user_tower
+from pydantic import BaseModel
+from typing import List
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+class NewUserRequest(BaseModel):
+    genres: List[str]
+
+# we need to create an embedding using the user tower for the
+# selected top 3 genres for cold start recommendations
+@router.post("/genre_embedding/{userId}")
+async def new_user(
+    userId: str,
+    body: NewUserRequest,
+    session: AsyncSession = Depends(get_session),
+    user_tower: ColdStartUserTower = Depends(get_cold_start_user_tower)
+):
+    #test_top3_genre = ["Science Fiction", "Adventure", "Comedy"]
+    genres = body.genres
+    user_embedding = user_tower.embedding(genres)
+
+    result = await new_user_genre_embedding(session, userId, user_embedding)
+
+    return result
+
+# we need to create an embedding using the user tower for the
+# selected top 3 genres for cold start recommendations
+@router.get("/create/{userId}")
+async def new_user(
+    userId: str,
+    session: AsyncSession = Depends(get_session),
+):
+    result = await test(session, userId)
+
+    return result
