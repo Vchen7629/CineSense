22a6aafd64025b431e1ab45a9ca6f989f90d5a31
Vchen7629 <chenvincent7629@gmail.com>
2025-11-16 00:16:40 -0800

    feat(frontend): sign up confirm button now calls the recommendations with the selected 3 genres to create the user embeddings

---
+++ frontend/src/app/types/user.ts
@@ -0,0 +1,5 @@
+
+export interface createGenreEmbProps {
+    user_id: string
+    genres: string[]
+}
--- frontend/src/features/user/components/SignUpConfirmButton.tsx
+++ frontend/src/features/user/components/SignUpConfirmButton.tsx
@@ -1,17 +1,22 @@
+import { CreateGenreEmbeddings } from "@/app/utils/user";
+import { useCreateGenreEmbeddings } from "../hooks/useCreateGenreEmbedding";
+
 interface SignUpConfirmButtonProps {
     selectedGenres: string[]
     canSignup: boolean
 }
 
 export const SignUpConfirmButton = ({ selectedGenres, canSignup }: SignUpConfirmButtonProps) => {
-
-    function handleClick() {
-        console.log(selectedGenres);
-    }
+    const test_user_id = "11"
+    const createGenreEmbeddings = useCreateGenreEmbeddings();
+    
+    const handleClick = () => {
+        createGenreEmbeddings(test_user_id, selectedGenres);
+    };
 
     return (
         <button
-            disabled={canSignup}
+            disabled={!canSignup}
             className={`${canSignup ? "bg-teal-600 hover:bg-teal-700" : "bg-gray-700"} w-28 px-4 py-2 rounded-md transition-colors duration-250`}
             onClick={handleClick}
         >
