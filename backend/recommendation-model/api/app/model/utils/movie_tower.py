b068b783eb2b5c9ae1497043408b360d2d21b152
Vchen7629 <chenvincent7629@gmail.com>
2025-11-21 01:50:28 -0800

    feat(backend): updated movie_tower to work with the new embeddings

--- backend/recommendation-model/api/app/model/utils/movie_tower.py
+++ backend/recommendation-model/api/app/model/utils/movie_tower.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as f
 import numpy as np
 from sentence_transformers import SentenceTransformer
+from fastapi import HTTPException
 from typing import List
 import joblib
 import os
@@ -15,72 +16,133 @@ class MovieTower:
         current_dir = os.path.dirname(__file__)
 
         if large_dataset:
-            model_dir = os.path.join(current_dir, "..", "files")
+            self.model_dir = os.path.join(current_dir, "..", "files")
         else:
-            model_dir = os.path.join(current_dir, "..", "files_small")
-
-        # Load trained movie tower state dict
-        tower_path = os.path.join(model_dir, "movie_tower.pth")
-        state_dict = torch.load(tower_path, weights_only=True, map_location=device)
-
-        # Extract shapes from saved weights
-        title_in_features = state_dict['title_linear.weight'].shape[1]  # Should be 384
-        genre_in_features = state_dict['genre_linear.weight'].shape[1]  # Number of genres
-
-        # Create linear layers with correct dimensions
-        self.title_linear = nn.Linear(title_in_features, embedding_dim, device=device)
-        self.genre_linear = nn.Linear(genre_in_features, embedding_dim, device=device)
-        self.projector = nn.Linear(embedding_dim * 2, embedding_dim, device=device)
-
-        # Load trained weights
-        self.title_linear.load_state_dict({
-            'weight': state_dict['title_linear.weight'],
-            'bias': state_dict['title_linear.bias']
-        })
-        self.genre_linear.load_state_dict({
-            'weight': state_dict['genre_linear.weight'],
-            'bias': state_dict['genre_linear.bias']
-        })
-        self.projector.load_state_dict({
-            'weight': state_dict['projector.weight'],
-            'bias': state_dict['projector.bias']
-        })
-
-        # Set to eval mode
-        self.title_linear.eval()
-        self.genre_linear.eval()
-        self.projector.eval()
+            self.model_dir = os.path.join(current_dir, "..", "files_small")
 
         self.relu = nn.ReLU()
 
         # Load preprocessing tools
-        self.title_encoder = SentenceTransformer("intfloat/multilingual-e5-small")
+        self.sentence_transformer_encoder = SentenceTransformer("intfloat/multilingual-e5-small")
 
         # Load the same MultiLabelBinarizer used for training
-        genre_mlb_path = os.path.join(model_dir, "genre_mlb.joblib")
+        genre_mlb_path = os.path.join(self.model_dir, "genre_mlb.joblib")
         self.genre_mlb = joblib.load(genre_mlb_path)
 
+        # loading model weights and layers when initialize
+        self._extract_feature_dims()
+        self._linear_layers()
+        self._load_trained_weights()
+
+    def _extract_feature_dims(self) -> None:
+        """ Loads trained movie tower state dict """
+        tower_path = os.path.join(self.model_dir, "movie_tower.pth")
+        self.state_dict = torch.load(tower_path, weights_only=True, map_location=self.device)
+
+        # Extract shapes from saved weights
+        self.title_in_features = self.state_dict['title_linear.weight'].shape[1]  # Should be 384
+        self.genre_in_features = self.state_dict['genre_linear.weight'].shape[1]  # Number of genres
+        self.year_in_features = self.state_dict['year_linear.weight'].shape[1]
+        self.metadata_sentence_in_features = self.state_dict['metadata_sentence_linear.weight'].shape[1]
+    
+    def _load_trained_weights(self) -> None:
+        """ Load trained weights for movie tower"""
+        # Load trained weights
+        self.title_linear.load_state_dict({
+            'weight': self.state_dict['title_linear.weight'],
+            'bias': self.state_dict['title_linear.bias']
+        })
+        self.genre_linear.load_state_dict({
+            'weight': self.state_dict['genre_linear.weight'],
+            'bias': self.state_dict['genre_linear.bias']
+        })
+        self.year_linear.load_state_dict({
+            'weight': self.state_dict['year_linear.weight'],
+            'bias': self.state_dict['year_linear.bias']
+        })
+        self.metadata_sentence_linear.load_state_dict({
+            'weight': self.state_dict['metadata_sentence_linear.weight'],
+            'bias': self.state_dict['metadata_sentence_linear.bias']
+        })
+        self.projector.load_state_dict({
+            'weight': self.state_dict['projector.weight'],
+            'bias': self.state_dict['projector.bias']
+        })
+    
+    def _linear_layers(self) -> None:
+        # Create linear layers with correct dimensions
+        self.title_linear = nn.Linear(self.title_in_features, self.embedding_dim, device=self.device)
+        self.genre_linear = nn.Linear(self.genre_in_features, self.embedding_dim, device=self.device)
+        self.year_linear = nn.Linear(self.year_in_features, self.embedding_dim, device=self.device)
+        self.metadata_sentence_linear = nn.Linear(self.metadata_sentence_in_features, self.embedding_dim, device=self.device)
+        self.projector = nn.Linear(self.embedding_dim * 4, self.embedding_dim, device=self.device)
+
+        # Set to eval mode
+        self.title_linear.eval()
+        self.genre_linear.eval()
+        self.year_linear.eval()
+        self.metadata_sentence_linear.eval()
+        self.projector.eval()
+
     # generate embeddings for a new unseen movie using trained movie tower
-    def generate_new_movie_embedding(self, title: str, genres: List[str]) -> List[np.ndarray]:
+    def generate_new_movie_embedding(
+        self, 
+        title: str, 
+        genres: List[str], 
+        year: int, 
+        actors: List[str],
+        director: List[str],
+        overview: str
+    ) -> List[np.ndarray]:
         with torch.no_grad():
             # Encode title with sentence transformer
-            title_features = self.title_encoder.encode([title], convert_to_numpy=True)
+            title_features = self.sentence_transformer_encoder.encode([title], convert_to_numpy=True)
             title_tensor = torch.tensor(title_features, dtype=torch.float32, device=self.device)
 
             # Encode genres with mlb
             genre_features = self.genre_mlb.transform([genres])
             genre_tensor = torch.tensor(genre_features, dtype=torch.float32, device=self.device)
 
+            # encode movie year with normalized year
+            year_normalized = (year - 1900) / 125.0 # normalizes it between [0, 1] in a float
+            year_tensor = torch.tensor([[year_normalized]], dtype=torch.float32, device=self.device)
+
+            # Encode actors/director/movie overview into one sentence embedding using sentence transformers
+            metadata_parts = []
+
+            if not overview:
+                raise HTTPException(status_code=500, detail="missing movie overview")
+            if not director:
+                raise HTTPException(status_code=500, detail="missing movie director")
+            if not actors:
+                raise HTTPException(status_code=500, detail="missing movie actors")
+            
+            # add movie overview
+            metadata_parts.append(overview)
+            # add movie director with identifying string
+            metadata_parts.append(f"Directed by {director}")
+            # add movie actors with identifying string
+            metadata_parts.append(f"Starring {actors}")
+
+            metadata_sentence = ". ".join(metadata_parts).strip(". ")
+
+            metadata_sentence_features = self.sentence_transformer_encoder.encode([metadata_sentence], convert_to_numpy=True)
+            metadata_sentence_tensor = torch.tensor(metadata_sentence_features, dtype=torch.float32, device=self.device)
+
             # Pass through trained linear layers
             title_emb = self.relu(self.title_linear(title_tensor))
             genre_emb = self.relu(self.genre_linear(genre_tensor))
+            year_emb = self.relu(self.year_linear(year_tensor))
+            metadata_sentence_emb = self.relu(self.metadata_sentence_linear(metadata_sentence_tensor))
 
             # Normalize embeddings
             title_emb = f.normalize(title_emb, p=2, dim=1)
             genre_emb = f.normalize(genre_emb, p=2, dim=1)
+            year_emb = f.normalize(year_emb, p=2, dim=1)
+            metadata_sentence_emb = f.normalize(metadata_sentence_emb, p=2, dim=1)
 
             # Concatenate and project to final embedding
-            combined_emb = torch.cat([title_emb, genre_emb], dim=1)
+            combined_emb = torch.cat([title_emb, genre_emb, year_emb, metadata_sentence_emb], dim=1)
             final_emb = self.projector(combined_emb)
             final_emb = f.normalize(final_emb, p=2, dim=1)
 
