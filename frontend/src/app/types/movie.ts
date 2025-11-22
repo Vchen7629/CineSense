0c4732ceac004de81e0dbccaf7b1cd1749265aa2
Vchen7629 <chenvincent7629@gmail.com>
2025-11-21 21:01:13 -0800

    chore(frontend): updated types and api request body to send the required data for reranker model

--- frontend/src/app/types/movie.ts
+++ frontend/src/app/types/movie.ts
@@ -1,13 +1,16 @@
 
 export interface RateMovieApi {
     movie_id: string,
-    user_id: string,
-    name: string,
+    user_id?: string,
+    title: string,
     genres: string[],
     release_date: number,
     summary: string,
     actors: string[],
     director: string[],
     poster_path: string
-    rating: number
+    rating?: number,
+    tmdb_vote_avg: number,
+    tmdb_vote_count: number,
+    tmdb_popularity: number
 }
