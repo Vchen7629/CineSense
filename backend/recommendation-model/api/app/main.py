54ea2f8932aa9816b99fad160627424253d7f3ce
Vchen7629 <chenvincent7629@gmail.com>
2025-11-25 01:18:03 -0800

    trigger changes

--- backend/recommendation-model/api/app/main.py
+++ backend/recommendation-model/api/app/main.py
@@ -70,7 +70,7 @@ async def base():
 
 @app.get("/health")
 async def health():
-    return {"status": "healthy"}
+    return {"status": "healthy!"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
