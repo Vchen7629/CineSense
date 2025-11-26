831c7e58273512f290991f170b4f6175ec3f093f
Vchen7629 <chenvincent7629@gmail.com>
2025-11-26 01:52:56 -0800

    refactor(backend): created lifespan function for user-api to handle gracefuul startup/shutdown of database connections

--- backend/user-api/app/main.py
+++ backend/user-api/app/main.py
@@ -1,28 +1,27 @@
 # Entry Point
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from sqlalchemy import inspect
 import uvicorn
-
+from db.config.conn import engine, async_session
+from sqlalchemy import text
 from routes import status, user
-from db.dbConn import engine
 
-from db.base import Base
+# lifespan for managing database connection/cleanup
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with async_session() as session:
+          await session.execute(text("SELECT 1"))
+    print("Database connection verified!")
+    yield
 
+    await engine.dispose()
 
 app = FastAPI()
 
 # Including routes from routes folder.
 app.include_router(status.router, prefix="", tags=["status"])
 app.include_router(user.router)
-
-Base.metadata.create_all(bind=engine)
-
-
-# printing out tables
-with engine.connect() as conn:
-    inspector = inspect(engine)
-    tables = inspector.get_table_names()
-    print("tables in DB:", tables)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
