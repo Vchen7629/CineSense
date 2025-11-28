0b6c194e4d2b1c8a16c64e4bb30d0fd61f2b56b3
vchen7629 <chenvincent7629@gmail.com>
2025-11-27 22:18:36 -0800

    fix(backend): returns actual session

--- backend/user-api/app/db/utils/auth_sql_queries.py
+++ backend/user-api/app/db/utils/auth_sql_queries.py
@@ -4,11 +4,16 @@ from models.sessions import Session
 import uuid
 from datetime import datetime, timedelta, timezone
 
-async def check_valid_session_token(session: AsyncSession, token: str):
+async def check_valid_session_token(db: AsyncSession, token: str):
     """Check db to see if the session exists, returns none if doesnt exist """
     query = select(Session).where(Session.session_token == token)
-    result = await session.execute(query)
-    session = result.scalar_one_or_none()
+    result = await db.execute(query)
+    session_record = result.scalar_one_or_none()
+
+    # check if session exists and hasn't expired
+    if session_record and session_record.expire_at > datetime.now(timezone.utc):
+        return session_record
+    return None
 
 async def create_session(user_id: int, db: AsyncSession) -> str:
     token = str(uuid.uuid4())
