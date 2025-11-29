8e5a1f08eb571d89d7ff0f7d3c6eb009b88df572
vchen7629 <chenvincent7629@gmail.com>
2025-11-29 03:17:04 -0800

    refactor(frontend): movie recommendation page redesign

--- frontend/src/features/recommendations/pages/RecommendationPage.tsx
+++ frontend/src/features/recommendations/pages/RecommendationPage.tsx
@@ -7,6 +7,7 @@ import { useRateMovie } from "@/shared/hooks/useRateMovie";
 import RateMovieStars from "@/shared/components/app/rateMovieStars";
 import RateMovieButton from "@/shared/components/app/rateMovieButton";
 import LoadingMovieRecommendationsSkeleton from "../components/loadingSkeleton";
+import { Dot } from "lucide-react";
 
 export default function RecommendationPage() {
     {/*I probebly don't have to do this? But I'm going to do it for now. Change this if its not needed*/}
@@ -53,11 +54,30 @@ export default function RecommendationPage() {
                     ) : (
                         <>
                             {/*This will be the movies name and the body of the actual movie image*/}
-                            <section className="flex flex-col relative items-center pt-[2.5vh] h-[32.5rem] w-[30rem] bg-[#394B51] shadow-md shadow-black rounded-xl">
-                                <span className="bold text-white text-3xl font-semibold">
-                                    {currentMovie.title}
-                                </span>
-                                <div className="flex space-x-2 absolute bottom-6">
+                            <section className="flex flex-col relative justify-center items-center h-[32.5rem] w-[30rem]">
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w500${currentMovie.poster_path}`}
+                                    className="h-[99%] w-auto object-contain rounded-xl shadow-lg shadow-md shadow-black border-2 border-[#20363e]"
+                                    alt={currentMovie.title}
+                                />
+                            </section>
+                            <section className="flex flex-col space-y-[1vh] relative bg-[#394B51] h-[32.5rem] w-[50.5rem] px-[0.5vw] py-[2.5vh] rounded-xl shadow-md shadow-black rounded-xl">
+                                <div className="flex items-center justify-between w-[96%]">
+                                    <span className="bold text-white text-left text-3xl font-semibold ml-[1vw]">
+                                        {currentMovie.title}
+                                    </span>
+                                    <div className="flex space-x-2 items-center">
+                                        <div className="inline-flex items-center h-5 bg-yellow-500/20 backdrop-blur-sm text-amber-100 text-xs font-semibold rounded-full px-2.5        
+                                            leading-none border border-amber-400/50 shadow-sm hover:bg-yellow-500/30 transition-colors">
+                                            Avg rating: {currentMovie.tmdb_avg_rating.toFixed(1)}
+                                        </div>
+                                        <div className="inline-flex items-center h-5 bg-yellow-500/20 backdrop-blur-sm text-amber-100 text-xs font-semibold rounded-full px-2.5        
+                                            leading-none border border-amber-400/50 shadow-sm hover:bg-yellow-500/30 transition-colors">
+                                            Popularity: {currentMovie.tmdb_popularity.toFixed(1)}
+                                        </div>
+                                    </div>
+                                </div>
+                                 <div className="flex space-x-2 ml-[2vw] mt-2">
                                     <RateMovieStars rating={rating} setRating={setRating} />
                                     <RateMovieButton 
                                         onClick={handleMovieRated}
@@ -65,75 +85,82 @@ export default function RecommendationPage() {
                                         isError={ratingError}
                                         isSuccess={ratingSuccess}
                                     />
-                                </div>
-                                
-                            </section>
-
-                            {/*background flavor-image for description (Don't touch)*/}
-                            <section className="flex flex-col relative bg-[#394B51] h-[32.5rem] w-[50.5rem] px-[0.5vw] py-[2.5vh] rounded-xl shadow-md shadow-black rounded-xl">
-                                {/*Publisher - who made the movie here.*/}
-                                <div className="flex ml-[12px] space-x-[20px]">
-                                    <span className="text-white  text-3xl text-[25px]"> Directors: </span>
-                                    {currentMovie.director.slice(0, 4).map((director: string, idx: number) => (
-                                        <div 
-                                            key={idx}
-                                            className="flex items-center bg-teal-500/20 text-teal-200 shadow-inner hover:bg-teal-800 text-md rounded-xl border-teal-400 px-3 py-1 border-[0.1rem]"
-                                        >
-                                            {director}
-                                        </div>
-                                    ))}
-                                    {currentMovie.director.length > 4 && (
-                                        <button className="text-teal-400 text-[20px] hover:text-teal-300">
-                                            +{currentMovie.director.length - 5} more
-                                        </button>
-                                    )}
-                                </div>
-                                
-                                {/*Actors*/}
-                                <div className="flex ml-[12px] space-x-[20px] mt-[20px]">
-                                    <span className="text-white text-3xl text-[25px] mr-11"> Actors: </span>
-                                    {currentMovie.actors.slice(0, 4).map((actor: string, idx: number) => (
-                                        <div 
-                                            key={idx}
-                                            className="flex items-center bg-teal-500/20 text-teal-200 shadow-inner hover:bg-teal-800 text-md rounded-xl border-teal-400 px-3 py-1 border-[0.1rem]"
-                                        >
-                                            {actor}
-                                        </div>
-                                    ))}
-                                    {currentMovie.actors.length > 4 && (
-                                        <button className="text-teal-400 text-[20px] hover:text-teal-300">
-                                            +{currentMovie.actors.length - 5} more
-                                        </button>
-                                    )}
                                     </div>
-                                {/*Extra Box for detail under description (background)*/}
-                                <div className="bg-[#375367] h-[19.7rem] text-start p-4 mt-[20px] w-[48rem] rounded-[15px] ml-[8px] border-[0.1rem] border-[#20363e]">
-                                    {/*Description - what is this movie about.*/}
-                                    <p className="text-white text-3xl text-[20px]">{currentMovie.summary} </p>
-                                </div>
-
-                                <div className="flex w-full items-center mt-[20px] px-4 justify-between">
+                                <div className="flex items-center ml-[2vw]">
                                     {/*Date - when was it made*/}
-                                    <div className="flex items-center justify-center ml-[-9px] bg-[#375367] h-[2.6rem] px-4 rounded-[15px] border-[0.1rem] border-[#20363e]">
-                                        <p className="text-teal-200 shadow-inner text-lg rounded-xl border-teal-400  px-3 py-1">{currentMovie.release_date}</p>
-                                    </div>
-                                    {/*Genere*/}
-                                    <div className="flex space-x-4 px-4 bg-[#375367] h-[2.6rem] py-1 w-fit rounded-[15px] border-[0.1rem] border-[#20363e]">
+                                    <span 
+                                        className="inline-block bg-sky-500/20 backdrop-blur-sm text-teal-200 text-xs font-semibold rounded-full px-2.5 py-0.5 border
+                                                    border-teal-400/50 shadow-sm hover:bg-sky-500/30 transition-colors">
+                                        {currentMovie.release_date}
+                                    </span>
+                                    <Dot size={28}/>
+                                    {/*Languages*/}
+                                    <span 
+                                        className="inline-block bg-sky-500/20 backdrop-blur-sm text-teal-200 text-xs font-semibold rounded-full px-2.5 py-0.5 border
+                                                    border-teal-400/50 shadow-sm hover:bg-sky-500/30 transition-colors">
+                                        Language 1 | Language 2 
+                                    </span>
+                                    <Dot size={28}/>
+                                    <div className="flex space-x-2 w-fit">
                                         {currentMovie.genres.map((genre: string, idx: number) => (
                                             <div 
                                                 key={idx}
-                                                className="flex items-center bg-teal-500/20 text-teal-200 shadow-inner hover:bg-teal-800 text-sm rounded-xl border-teal-400 px-3 border-[0.1rem]"
-                                            >
+                                                className="inline-block bg-sky-500/20 backdrop-blur-sm text-teal-200 text-xs font-semibold rounded-full px-2.5 py-0.5 border
+                                                        border-teal-400/50 shadow-sm hover:bg-sky-500/30 transition-colors">
                                                 {genre}
                                             </div>
                                         ))}
                                     </div>
-                                    {/*Languages*/}
-                                    <div className="flex items-center justify-center bg-[#375367] h-[2.6rem] w-[8rem] rounded-[15px] border-[0.1rem] border-[#20363e]">
-                                        <p className="bg-teal-500/20 text-teal-200 shadow-inner hover:bg-teal-800 text-sm rounded-xl border-teal-400 px-3 py-1 border-[0.1rem]">
-                                        Languages
-                                        </p>
+                                </div>
+                                {/*Publisher - who made the movie here.*/}
+                                <div className="flex flex-col ml-[2vw] space-x-[20px]">
+                                    <span className="text-left text-lg font-semibold text-gray-300"> Directors: </span>
+                                    <div className="flex space-x-2 mt-2">
+                                        {currentMovie.director.slice(0, 4).map((director: string, idx: number) => (
+                                           <div
+                                                key={idx}
+                                                className="inline-flex items-center bg-teal-500/20 text-teal-200 text-sm font-medium rounded-full border border-teal-400/50 px-3      
+                                                    py-1 shadow-sm hover:bg-teal-500/30 transition-colors"
+                                            >
+                                                {director}
+                                            </div>
+                                        ))}
+                                        {currentMovie.director.length > 4 && (
+                                            <button className="inline-flex items-center bg-teal-500/20 text-teal-200 text-sm font-medium rounded-full border border-teal-400/50 px-3      
+                                                    py-1 shadow-sm hover:bg-teal-500/30 transition-colors"
+                                            >
+                                                +{currentMovie.director.length - 5} more
+                                            </button>
+                                        )}
                                     </div>
+                                </div>
+                                
+                                {/*Actors*/}
+                                <div className="flex flex-col ml-[2vw] space-x-[20px] ">
+                                    <span className="text-left text-lg font-semibold text-gray-300"> Actors: </span>
+                                    <div className="flex space-x-2 mt-2">
+                                        {currentMovie.actors.slice(0, 5).map((actor: string, idx: number) => (
+                                            <div 
+                                                key={idx}
+                                                className="inline-flex items-center bg-teal-500/20 text-teal-200 text-sm font-medium rounded-full border border-teal-400/50 px-3      
+                                                    py-1 shadow-sm hover:bg-teal-500/30 transition-colors"
+                                            >
+                                                {actor}
+                                            </div>
+                                        ))}
+                                        {currentMovie.actors.length > 5 && (
+                                            <button className="inline-flex items-center bg-teal-500/20 text-teal-200 text-sm font-medium rounded-full border border-teal-400/50 px-3      
+                                                    py-1 shadow-sm hover:bg-teal-500/30 transition-colors"
+                                            >
+                                                +{currentMovie.actors.length - 5} more
+                                            </button>
+                                        )}
+                                    </div>
+                                </div>
+                                {/*background flavor-image for description (Don't touch)*/}
+                                <div className="bg-[#375367] h-[19.7rem] text-start px-4 py-2 mt-[20px] w-[92.5%] rounded-[15px] ml-[2vw] border-[0.1rem] border-[#20363e]">
+                                    {/*Description - what is this movie about.*/}
+                                    <p className="text-white text-md">{currentMovie.summary} </p>
                                 </div>
                             </section>
                         
