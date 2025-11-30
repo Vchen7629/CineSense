5cd04edf3b718b70e545e14096c684f9a337a755
Vchen7629 <chenvincent7629@gmail.com>
2025-11-30 02:18:08 -0800

    feat(frontend): created hook for frontend add to watchlist api call

--- frontend/src/api/services/movie.ts
+++ frontend/src/api/services/movie.ts
@@ -70,5 +70,54 @@ export const MovieService = {
                 throw error;
             }
         }
+    },
+
+    // get user movie watchlist
+    getWatchlist: async({ user_id }: { user_id: string }) => {
+        try {
+            const response = await recommendations_api.get(`user/watchlist/get/${user_id}`)
+            
+            return response.data
+        } catch (error: unknown) {
+            if (error instanceof AxiosError) {
+                console.error(error.response?.data || error.message);
+                throw error;
+            } else if (error instanceof Error) {
+                console.error(error.message);
+                throw error;
+            } else {
+                console.error(error);
+                throw error;
+            }
+        }
+    },
+
+    addToWatchlist: async({ user_id, movie_id, title, genres, release_date, summary, actors, director, poster_path, rating }: any) => {
+        try {
+            const response = await recommendations_api.post(`user/watchlist/add/${user_id}`, {
+                movie_id: movie_id,
+                title: title,
+                genres: genres,
+                release_date: release_date,
+                summary: summary,
+                actors: actors,
+                director: director,
+                poster_path: poster_path,
+                rating: rating
+            })
+
+            return response.data
+        } catch (error: unknown) {
+            if (error instanceof AxiosError) {
+                console.error(error.response?.data || error.message);
+                throw error;
+            } else if (error instanceof Error) {
+                console.error(error.message);
+                throw error;
+            } else {
+                console.error(error);
+                throw error;
+            }
+        }
     }
 }
